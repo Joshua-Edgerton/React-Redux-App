e5564fd78ce69db8d54d1a8632c365a7f95a7956
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-11-07 22:08:33 -0600

    MVP

---
+++ react-redux-app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import styled from "styled-components"
+import MovieList from "./components/MovieList"
+
+function App() {
+  return (
+    <AppContainer>
+     <h1>Studio Ghibli Films</h1>
+     <MovieList />
+    </AppContainer>
+  );
+}
+
+export default App;
+
+const AppContainer = styled.div`
+  text-align: center;
+  background: white;
+  margin: 0;
+  padding: 0;
+
+  h1 {
+    padding-top: 20px;
+  }
+`; 
+
